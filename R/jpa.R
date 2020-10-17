5ab118013d3a9879d4fef86401d24970b8bb94bb
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2020-10-17 01:59:03 +0000

    update

--- R/jpa.R
+++ R/jpa.R
@@ -27,7 +27,6 @@ set_rc_jpa <- function (file_name = "paper"){
     file.create(file.path(path, "README.md"), showWarnings = FALSE)
     writeLines("## README\n\n- analysis:\n- data:\n- function:\n- materials:", "README.md")
   }
-
   # make RMarkdown file and directory
   if(!file.exists(file.path(path, paste0(file_name,".Rmd")))){
     rmarkdown::draft(paste0(file_name,".Rmd"), template = "jpa_jjp", package = "jpaRmd", edit = FALSE)
