7c5c31b9036a1ba2264d075bc8c30eb2dcab669a
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2020-10-18 02:56:08 +0000

    update

--- R/jpa.R
+++ R/jpa.R
@@ -17,45 +17,46 @@ jpa_jjp <- function(){
 #'
 #' @param file_name file name and directory name of RMarkdown
 #' @return Make directories for Research Compendium of JPA and R Markdown file
-#' @examples # set_rc_jpa("yoshihiko_kunisato")
+#' @examples # set_rc_jpa("rmarkdown_for_reproducibility")
 #' @export
 
 set_rc_jpa <- function (file_name = "paper"){
   path = getwd()
-  # make README
-  if(!file.exists(file.path(path, "README.md"))){
-    file.create(file.path(path, "README.md"), showWarnings = FALSE)
-    writeLines("## README\n\n- analysis:\n- data:\n- function:\n- materials:", "README.md")
+  # make Directory & README
+  if(!dir.exists(file.path(path, file_name))){
+    dir.create(file.path(path, file_name), showWarnings = FALSE)
+    file.create(file.path(path, file_name, "README.md"), showWarnings = FALSE)
+    writeLines("## README\n\n- analysis:\n- data:\n- function:\n- materials:", file.path(path, file_name, "README.md"))
   }
   # make RMarkdown file and directory
-  if(!file.exists(file.path(path, paste0(file_name,".Rmd")))){
-    rmarkdown::draft(paste0(file_name,".Rmd"), template = "jpa_jjp", package = "jpaRmd", edit = FALSE)
+  if(!file.exists(file.path(path, file_name, "paper.Rmd"))){
+    rmarkdown::draft(file.path(path, file_name, "paper.Rmd"), template = "jpa_jjp", package = "jpaRmd", edit = FALSE)
   }
   # make analysis directory
-  if(!dir.exists(file.path(path, "analysis"))){
-    dir.create(file.path(path, "analysis"), showWarnings = FALSE)
-    file.create(file.path(path, "analysis/README_analysis.md"), showWarnings = FALSE)
-    writeLines("README about analysis", "analysis/README_analysis.md")
+  if(!dir.exists(file.path(path, file_name, "analysis"))){
+    dir.create(file.path(path, file_name, "analysis"), showWarnings = FALSE)
+    file.create(file.path(path, file_name, "analysis/README_analysis.md"), showWarnings = FALSE)
+    writeLines("README about analysis", file.path(path, file_name, "analysis/README_analysis.md"))
   }
 
   # make data directory
-  if(!dir.exists(file.path(path, "data"))){
-    dir.create(file.path(path, "data"), showWarnings = FALSE)
-    file.create(file.path(path, "data/README_data.md"), showWarnings = FALSE)
-    writeLines("README about data", "data/README_data.md")
+  if(!dir.exists(file.path(path, file_name, "data"))){
+    dir.create(file.path(path, file_name, "data"), showWarnings = FALSE)
+    file.create(file.path(path, file_name, "data/README_data.md"), showWarnings = FALSE)
+    writeLines("README about data", file.path(path, file_name, "data/README_data.md"))
   }
 
   # make function directory
-  if(!dir.exists(file.path(path, "function"))){
-    dir.create(file.path(path, "function"), showWarnings = FALSE)
-    file.create(file.path(path, "function/README_function.md"), showWarnings = FALSE)
-    writeLines("README about function", "function/README_function.md")
+  if(!dir.exists(file.path(path, file_name, "function"))){
+    dir.create(file.path(path, file_name, "function"), showWarnings = FALSE)
+    file.create(file.path(path, file_name, "function/README_function.md"), showWarnings = FALSE)
+    writeLines("README about function", file.path(path, file_name, "function/README_function.md"))
   }
 
   # make materials directory
-  if(!dir.exists(file.path(path, "materials"))){
-    dir.create(file.path(path, "materials"), showWarnings = FALSE)
-    file.create(file.path(path, "materials/README_materials.md"), showWarnings = FALSE)
-    writeLines("README about materials", "materials/README_materials.md")
+  if(!dir.exists(file.path(path, file_name, "materials"))){
+    dir.create(file.path(path, file_name, "materials"), showWarnings = FALSE)
+    file.create(file.path(path, file_name, "materials/README_materials.md"), showWarnings = FALSE)
+    writeLines("README about materials", file.path(path, file_name, "materials/README_materials.md"))
   }
 }
--- README.md
+++ README.md
@@ -7,26 +7,25 @@
 
 <!-- badges: end -->
 
-jpaRmdは，日本心理学会の「心理学研究」への投稿論文をRmarkdownで作成するためのパッケージです。
+jpaRmdは，日本心理学会の『心理学研究』への投稿用PDF原稿をRmarkdownで作成するためのパッケージです。
 
-以下の日本心理学会の「執筆・投稿の手びき(2015年版)」に基づいています。
+以下の日本心理学会の「執筆・投稿の手びき(2015年版)」に基づいています。引用文献などはまだ完璧とは言い難いですが，投稿用原稿にほぼ近いものができると思います。
 
 <https://psych.or.jp/manual/>
-
-まだ全く完成していません・・・使用
 
 ## インストール
 
 以下のコマンドをRコンソールに打ち込んで，Github経由でインストールしてください。
 
-    # install.packages("devtools")
-    devtools::install_github("ykunisato/jpaRmd")
+    # install.packages("remotes")
+    remotes::install_github("ykunisato/jpaRmd")
 
 ## 使用法
 
   - RStudioで，「File」 -\> 「New File」 -\> 「R Markdown…」
     をクリックする。以下の画面がでてきたら，「From
-    Template」から「… {jpaRmd}」を選んで，OKをクリックする。開かれた.RmdファイルをKnitください。
+    Template」から「Japanese Psychological Association
+    format{jpaRmd}」を選んで，OKをクリックする。開かれた.RmdファイルをKnitください。
 
   - Research
     Compendiumの関数も用意していて，以下のようにset\_rc\_jpa()関数を使って，引数にプロジェクト名をいれると（なおスペースは避けてください），『心理学研究』用のRmdや解析，データを配置するフォルダなども準備されます。
