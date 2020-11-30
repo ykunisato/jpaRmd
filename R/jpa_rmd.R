d7ec12e9e1b2ee8159858ddfc67e172898905371
ykunisato <ykunisato@gmail.com>
2020-11-30 02:21:48 +0000

    add jjbct format

--- R/jpa_rmd.R
+++ R/jpa_rmd.R
@@ -16,6 +16,25 @@ jpa_jjp <- function(){
   format_pdf
 }
 
+#' R Markdown template for paper of Japanese Journal of Behavioral and Cognitive Therapies
+#' 
+#' @export
+#' @importFrom rmarkdown pdf_document
+
+jjbct <- function(){
+  template_tex_file <- system.file("rmarkdown/templates/jjbct/resources/jjbct.tex",
+                                   package = 'jpaRmd')
+  format_pdf <- pdf_document(latex_engine = "xelatex",
+                             template = template_tex_file,
+                             keep_tex = TRUE,
+                             toc = TRUE,
+                             toc_depth = 3,
+                             highlight = 'tango')
+  format_pdf$inherits <- "pdf_document"
+  format_pdf
+}
+
+
 #' R Markdown template for paper of the Japanese Journal of Psychology ver2
 #' @export
 #' @importFrom rmarkdown pdf_document
